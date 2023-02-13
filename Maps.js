2d2ca6fce1c1e1a55ca994c2a92cd78386ced626
matthewdavidr12 <matthewdavidr12@hotmail.com>
2023-02-12 20:14:42 -0500

    Trabajo

---
+++ Maps.js
@@ -0,0 +1,50 @@
+const emojis={
+
+    '-':' ',
+    'O':'🚪',
+    'X':'💩',
+    'I':'🦴',
+    'PLAYER':'🐶',
+    'BOMB_COLLISION':'🤮',
+    'GAME_OVER':'👎',
+    'WIN':'🎉',
+
+};
+
+const maps = [];
+maps.push(`
+  IXXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  OXXXXXXXXX
+`);
+maps.push(`
+  O--XXXXXXX
+  X--XXXXXXX
+  XX----XXXX
+  X--XX-XXXX
+  X-XXX--XXX
+  X-XXXX-XXX
+  XX--XX--XX
+  XX--XXX-XX
+  XXXX---IXX
+  XXXXXXXXXX
+  `);
+maps.push(`
+  I-----XXXX
+  XXXXX-XXXX
+  XX----XXXX
+  XX-XXXXXXX
+  XX-----XXX
+  XXXXXX-XXX
+  XX-----XXX
+  XX-XXXXXXX
+  XX-----OXX
+  XXXXXXXXXX
+`);
